2677f5532662fe01b90a2f90c46670596a25d598
yisus <minecraftgamingra@gmail.com>
2025-03-03 11:16:06 +0100

    test

--- mandatory/src/main.c
+++ mandatory/src/main.c
@@ -14,31 +14,9 @@
 
 int	main(int argc, char **argv)
 {
-	t_game	game;
-
-	if (argc != 2)
+	(void)argv;
+	if (argc == 2)
 	{
-		ft_putstr_fd("Error\nInvalid number of arguments\n", 2);
-		return (1);
+		printf("test");
 	}
-	if (read_map(argv[1], &game.map) == -1)
-	{
-		ft_putstr_fd("Error\nInvalid map\n", 2);
-		return (1);
-	}
-	game.mlx = mlx_init();
-	if (!game.mlx)
-	{
-		ft_putstr_fd("Error\nmlx_init() failed\n", 2);
-		return (1);
-	}
-	if (init_game(&game) == -1)
-	{
-		ft_putstr_fd("Error\ninit_game() failed\n", 2);
-		return (1);
-	}
-	mlx_hook(game.win, 2, 1L << 0, key_hook, &game);
-	mlx_hook(game.win, 17, 1L << 17, close_window, &game);
-	mlx_loop(game.mlx);
-	return (0);
 }
