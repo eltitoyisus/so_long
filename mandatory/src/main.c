b22e0fc98d754fcc363d2e3a5b76b42fe2de834f
yisus <minecraftgamingra@gmail.com>
2025-03-05 20:20:35 +0100

    test

--- mandatory/src/main.c
+++ mandatory/src/main.c
@@ -14,20 +14,23 @@
 
 int	main(int argc, char **argv)
 {
-	t_map	map;
 	t_game	game;
 
-	if (argc == 2)
+	if (argc != 2)
 	{
-		map_initializer(&map, argv);
-		map_checker(&map);
-		game.mlx = mlx_init();
-		game.win = mlx_new_window(game.mlx, map.player_x * IMG_PXL, map.player_y * IMG_PXL, WND_NAME);
-		file_to_image(&map);
-		map_printer(&map);
-		mlx_hook(game.win, 17, 0, ft_close_map, &map);
-		mlx_key_hook(game.win, key_hook, &map);
-		mlx_loop(game.mlx);
+		ft_putstr_fd("Error\nInvalid number of arguments\n", 2);
+		return (1);
 	}
-	exit(EXIT_FAILURE);
+	init_map(&game.map);
+	if (parse_map(argv[1], &game.map) == -1)
+	{
+		ft_putstr_fd("Error\nInvalid map\n", 2);
+		return (1);
+	}
+	init_game(&game);
+	init_img(&game);
+	mlx_hook(game.win, 2, 1L << 0, key_hook, &game);
+	mlx_hook(game.win, 17, 1L << 17, ft_close_map, &game);
+	mlx_loop(game.mlx);
+	return (0);
 }
